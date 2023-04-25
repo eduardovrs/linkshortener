afdb1416f33acf5925e88b05988f83eda2114ac9
Eduardo Vinicius <edu.vinirodrigues@gmail.com>
2023-04-25 18:26:27 -0300

    🏷️ Add clipBoardURL prop to interface

--- src/components/ClipboardModal/ClipBoardModal.structure.ts
+++ src/components/ClipboardModal/ClipBoardModal.structure.ts
@@ -1,5 +1,6 @@
 export interface IClipBoardModalProps {
   isModalOpened: boolean;
   setIsModalOpened: React.Dispatch<React.SetStateAction<boolean>>;
+  clipBoardURL: string;
   onPress: () => void;
 }
