ad3083185951f96871be6f5d20b94262c464ab1a
Eduardo Vinicius <edu.vinirodrigues@gmail.com>
2023-04-25 18:27:41 -0300

    💄 Adjust styles due to new text on clipboard modal

--- src/components/ClipboardModal/styles.ts
+++ src/components/ClipboardModal/styles.ts
@@ -7,18 +7,28 @@ export const ContainerModal = styled.View`
   border-top-right-radius: 16px;
   border-top-left-radius: 16px;
   flex-direction: column;
-  top: 70%;
+  top: 60%;
 `;
 
 export const ModalTitle = styled.Text`
   font-size: 22px;
   font-weight: bold;
-  margin: 10% 0 5% 0;
+  color: #000000;
+  margin: 10% 0 0 0;
+`;
+
+export const UrlText = styled.Text`
+  width: 90%;
+  font-size: 20px;
+  font-weight: 500;
+  margin: 5% 0 5% 0;
+  color: #409eff;
 `;
 
 export const ModalSubtitle = styled.Text`
   font-size: 18px;
   font-weight: 600;
+  color: #000000;
 `;
 
 export const ButtonView = styled.View`
@@ -46,4 +56,5 @@ export const DeclineButton = styled.TouchableOpacity`
 export const ButtonText = styled.Text`
   font-size: 20px;
   font-weight: bold;
+  color: #000000;
 `;
